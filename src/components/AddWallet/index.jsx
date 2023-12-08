6dc2ac6add91736b2902ff7ad3ab9a6915c27357
RJ <89183490+rajnishfrost@users.noreply.github.com>
2023-12-08 17:49:27 +0530

    added functionality of import wallet and create wallet

--- src/components/AddWallet/index.jsx
+++ src/components/AddWallet/index.jsx
@@ -1,44 +1,85 @@
-import { VerifiedWallet } from '@verified-network/verified-sdk';
-import React from 'react';
-import { Container } from '../Container';
-import { Heading } from '../Heading';
-import { Button } from '../Button';
-import { useWalletContext } from '../../store/wallet/walletContext';
+import { VerifiedWallet } from "@verified-network/verified-sdk";
+import React, { useState } from "react";
+import { Container } from "../Container";
+import { Heading } from "../Heading";
+import { Button } from "../Button";
+import { useWalletContext } from "../../store/wallet/walletContext";
 
 export const AddWallet = () => {
   const { setVerifiedWallet, setEthAddress } = useWalletContext();
+  const [imports, setImport] = useState(false);
+  const [mnemonics, setMnemonics] = useState("");
 
-  const handleCreateWallet = () => {
+  const handleCreateWallet = async () => {
     const wallet = VerifiedWallet.createWallet();
-
+    alert(
+      `Wallet Created Successfully, Save this Mnemonoics[${
+        wallet._mnemonic().phrase
+      }]`
+    );
     setVerifiedWallet(wallet);
     setEthAddress(wallet.address);
   };
 
-  const handleImportMnemonicsWallet = async () => {
-    const mnemonics = await VerifiedWallet.generateMnemonic();
-    const wallet = VerifiedWallet.importWallet(mnemonics);
-
-    setVerifiedWallet(wallet);
-    setEthAddress(wallet.address);
+  const handleImportMnemonicsWallet = () => {
+    try {
+      const wallet = VerifiedWallet.importWallet(mnemonics);
+      setVerifiedWallet(wallet);
+      setEthAddress(wallet.address);
+      alert("Wallet Imported Successfully please press OK")
+    } catch (error) {
+      alert("invalid mnemonics");
+    }
   };
 
   return (
     <Container>
-      <Heading title='Add New Wallet' />
-
-      <div className='flex gap-4 items-center justify-center w-full h-[calc(100%-80px)]'>
-        <Button
-          title='Import Mnemonics Wallet'
-          variant='error'
-          onClick={handleImportMnemonicsWallet}
-        />
-        <Button
-          title='Create Wallet'
-          variant='warning'
-          onClick={handleCreateWallet}
-        />
-      </div>
+      <Heading title="Add New Wallet" />
+      {imports ? (
+        <>
+          <input
+            onChange={(e) => setMnemonics(e.target.value)}
+            type="text"
+            style={{
+              color: "black",
+              padding: "0px 10px",
+              width: "100%",
+              border: "2px solid grey",
+              marginTop: "50px",
+              borderRadius: "100px",
+            }}
+          />
+          <button
+            onClick={() => handleImportMnemonicsWallet()}
+            style={{
+              margin: "auto",
+              marginTop: "20px",
+              display: "block",
+              padding: "10px",
+              background: "blue",
+              width: "100px",
+              borderRadius: "100px",
+            }}
+          >
+            Import
+          </button>
+        </>
+      ) : (
+        <>
+          <div className="flex gap-4 items-center justify-center w-full h-[calc(100%-80px)]">
+            <Button
+              title="Import Mnemonics Wallet"
+              variant="error"
+              onClick={() => setImport(true)}
+            />
+            <Button
+              title="Create Wallet"
+              variant="warning"
+              onClick={handleCreateWallet}
+            />
+          </div>
+        </>
+      )}
     </Container>
   );
 };
